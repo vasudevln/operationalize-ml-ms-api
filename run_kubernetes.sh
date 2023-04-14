bc25a58e2c5430ce3d61e08ae6f6087d2daec67b
Vasu Dev L <vasudev@kloudone.com>
2023-04-14 22:08:49 +0530

    added step 5 files

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,17 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=vasudevla/ml-app
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl create deploy ml-app --image=vasudevla/ml-app:v1.0.0
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward pod/ml-app-77d7767ff9-mhrl9 --address 0.0.0.0 8000:80
 
